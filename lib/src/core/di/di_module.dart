7fef4cf6f25a84bf6fc72422af8e1a11b9d6bdd7
Andres Escobar <andres.escobar.9103@gmail.com>
2024-03-03 16:48:12 +0100

    Use pretty logger for dio

--- lib/src/core/di/di_module.dart
+++ lib/src/core/di/di_module.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 @module
 abstract class DioProvider {
@@ -9,7 +10,7 @@ abstract class DioProvider {
     Dio dio = Dio();
     if (kDebugMode) {
       dio.interceptors.add(
-        LogInterceptor(
+        PrettyDioLogger(
           responseBody: true,
           requestBody: true,
         ),
